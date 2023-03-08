c9a1d5c3887148ee3b71b4b423e5da944edaa467
David-Neto <davidrmachado@gmail.com>
2023-03-08 00:51:45 -0300

    Entrega do requisito 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,23 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, list):
+        simple_result = super().generate(list)
+        companies_list = []
+        companies = Counter(companies_list)
+        string = ""
+
+        for product in list:
+            companies_list.append(product["nome_da_empresa"])
+
+        for company in companies.most_common():
+            string += f'- {company[0]}: {company[1]}\n'
+
+        return (
+            f"{simple_result}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{string}s"
+        )
