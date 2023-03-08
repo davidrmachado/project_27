6731f35aabc70fcd6d5dab04531e31938dcd4ccb
David-Neto <davidrmachado@gmail.com>
2023-03-07 23:36:04 -0300

    Entrega do requisito 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,26 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, list):
+        expiration_date = []
+        manufacturing_date = []
+        current_date = str(datetime.now())
+        counter = Counter()
+
+        for info in list:
+            manufacturing_date.append(info["data_de_fabricacao"])
+            if info["data_de_validade"] > current_date:
+                expiration_date.append(info["data_de_validade"])
+            counter[info["nome_da_empresa"]] += 1
+
+        manufacturing_date.sort()
+        expiration_date.sort()
+
+        return (
+            f"Data de fabricação mais antiga: {manufacturing_date[0]}\n"
+            f"Data de validade mais próxima: {expiration_date[0]}\n"
+            f"Empresa com mais produtos: {counter.most_common()[0][0]}"
+        )
